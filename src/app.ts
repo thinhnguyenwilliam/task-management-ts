bb9b155dc442426486718bca955ec422eef5a401
ThinhNguyen <thinhproee@gmail.com>
2024-11-15 14:18:52 +0700

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 // src/app.ts
 import express from "express";
 import { routesClient } from "./routes/client/indexRoute";
+import cors from 'cors';
 
 const app = express();
+
+// Define CORS options
+const corsOptions: cors.CorsOptions = {
+    origin: ['http://localhost:3000', 'https://example.com'], // Allowed origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true, // Allow cookies to be sent with cross-origin requests
+};
+
+// Enable CORS middleware
+app.use(cors(corsOptions));
 
 
 // Middleware setup
